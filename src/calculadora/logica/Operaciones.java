658619a0ed764c2fc3ddf9ecf4565301e2f7756c
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-12-06 11:39:05 +0100

    Feat: Añadida la funcionalidad de suma

--- src/calculadora/logica/Operaciones.java
+++ src/calculadora/logica/Operaciones.java
@@ -1,13 +1,10 @@
 package calculadora.logica;
 
-import javax.swing.JFrame;
 
-public class Operaciones extends JFrame{
+public class Operaciones{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6909078123543531767L;
-
-	 
+	public double sumar(double a, double b) {
+        return a + b;
+    }
+	
 }
