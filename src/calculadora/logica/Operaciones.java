39036e3c23ca700906f058725f280129d63d3e11
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-12-06 11:40:35 +0100

    Feat: Añadida la funcionalidad de division

--- src/calculadora/logica/Operaciones.java
+++ src/calculadora/logica/Operaciones.java
@@ -14,4 +14,11 @@ public class Operaciones{
 	public double multiplicar(double a, double b) {
         return a * b;
     }
+	
+	public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("División por cero no permitida");
+        }
+        return a / b;
+    }
 }
