830039b8a90a3c701f0ca323ebf2ba06531a47a4
Rodrigo Pascual Arnaiz <ronripro@gmail.com>
2024-12-06 11:42:24 +0100

    Chore: añadir comentarios

--- src/calculadora/logica/Operaciones.java
+++ src/calculadora/logica/Operaciones.java
@@ -3,18 +3,48 @@ package calculadora.logica;
 
 public class Operaciones{
 
+	/**
+	 * Suma dos números.
+	 * 
+	 * @param a el primer número.
+	 * @param b el segundo número.
+	 * @return la suma de a y b.
+	 */
 	public double sumar(double a, double b) {
         return a + b;
     }
 	
+
+	/**
+	 * Resta dos números.
+	 * 
+	 * @param a el número al que se le restará.
+	 * @param b el número que se restará a a.
+	 * @return el resultado de a - b.
+	 */
 	public double restar(double a, double b) {
         return a - b;
     }
 	
+	/**
+	 * Multiplica dos números.
+	 * 
+	 * @param a el primer número.
+	 * @param b el segundo número.
+	 * @return el producto de a y b.
+	 */
 	public double multiplicar(double a, double b) {
         return a * b;
     }
 	
+	/**
+	 * Divide dos números.
+	 * 
+	 * @param a el dividendo.
+	 * @param b el divisor. No debe ser cero.
+	 * @return el resultado de dividir a entre b.
+	 * @throws ArithmeticException si b es igual a 0, ya que no se puede dividir entre cero.
+	 */
 	public double dividir(double a, double b) {
         if (b == 0) {
             throw new ArithmeticException("División por cero no permitida");
